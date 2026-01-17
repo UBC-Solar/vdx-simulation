89534e9d12cb26bf961946d8fbd3841f05932a5c
Munro Anderson <munro@student.ubc.ca>
2026-01-17 10:42:55 -0800

    reorganize steering scripts

---
+++ global/iso2sw.m
@@ -0,0 +1,6 @@
+function swCoords = iso2sw(isoCoords)
+%iso2sw Convert ISO 8855 coordinates to SolidWorks
+%   TODO describe orientation
+
+swCoords = [isoCoords(2); isoCoords(3); isoCoords(1)];
+end
